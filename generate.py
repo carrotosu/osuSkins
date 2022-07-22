8edd4e41f15c1dfe53c94bf5499fc0fa09064079
eranyoung <eranyoung1@gmail.com>
2022-07-21 20:34:18 -0700

    screenshots

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
 import os
 import re
 import shutil
+import subprocess
 
 github_link = 'https://github.com/eranyoung/osuSkins/tree/master/Skins/' #change this to your github website
 osu_folder = '/Users/erany/AppData/Local/osu!' #change this to your osu! directory
 skins_dir = osu_folder + '/Skins'
+
+subprocess.call('C:/Users/erany/OneDrive/Desktop/osuskinthumbnailcreator/main.py', shell='True') #experimental, for generating images automatically, delete this line for just links
 
 def generate():
 	entries = os.listdir(skins_dir)
@@ -12,11 +15,13 @@ def generate():
 	f.write('# My Osu! Skins' + '\n')
 	for entry in entries:
 		filename = re.sub('\W+', '', entry)
-		f.write('\n## '+filename+'\n')
+		f.write('\n## '+filename+)
 		f.write('[Download](' + github_link+filename+'.osk' +')' )
 		if not os.path.isfile('Skins/' + filename+'.osk'):
+			print('writing ' + filename + "...")
 			shutil.make_archive(filename, 'zip', skins_dir + '/' + entry)
 			os.rename(filename+'.zip', filename+'.osk')
 			shutil.move(filename+'.osk', 'Skins')
 
 generate()
+print('done')
